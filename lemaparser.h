cb2789a0b2e94e71943af1b972a09901663cb5fd
jonathandrnd <jonathan.drnd@gmail.com>
2016-10-03 15:36:54 -0500

    Update lemaparser.h

--- lemaparser.h
+++ lemaparser.h
@@ -27,8 +27,36 @@ string PrintLemma(list<sentence> &ls) {
    return ans;
 }
 
-
-
+maco init(){
+    util::init_locale(L"default");
+    wstring ipath=L"/usr/local";
+    wstring path=ipath+L"/share/freeling/es/";
+    
+    maco_options opt(L"es");
+    opt.UserMapFile=L"";
+    opt.LocutionsFile=path+L"locucions.dat"; opt.AffixFile=path+L"afixos.dat";
+    opt.ProbabilityFile=path+L"probabilitats.dat"; opt.DictionaryFile=path+L"dicc.src";
+    opt.NPdataFile=path+L"np.dat"; opt.PunctuationFile=path+L"../common/punct.dat"; 
+    maco morfo(opt);
+    morfo.set_active_options (false,// UserMap
+                             true, // NumbersDetection,
+                             true, //  PunctuationDetection,
+                             true, //  DatesDetection,
+                             true, //  DictionarySearch,
+                             true, //  AffixAnalysis,
+                             false, //  CompoundAnalysis,
+                             true, //  RetokContractions,
+                             true, //  MultiwordsDetection,
+                             true, //  NERecognition,
+                             false, //  QuantitiesDetection,
+                             true);  //  ProbabilityAssignment
+    hmm_tagger tagger(path+L"tagger.dat", true, FORCE_TAGGER); 
+    // create chunker 
+    chart_parser parser(path+L"chunker/grammar-chunk.dat");
+    // create dependency parser 
+    dep_txala dep(path+L"dep_txala/dependences.dat", parser.get_start_symbol());
+    return morfo;
+}
 
 string parserlemma(string s,maco &morfo){
     wstring ipath=L"/usr/local";
